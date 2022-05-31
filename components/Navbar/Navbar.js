cf10584340f471bcd98ff7fe2993b004275f925b
Henok <henokmicrometre@gmail.com>
2022-05-31 02:45:06 +0100

    sorted css

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
+import Link from "next/link";
 import styles from "./Navbar.module.css"
 // Navbar.js
 export default function Navbar() {
+
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
   return (
     <nav className={styles.navigation}>
       <a href="/" className={styles.brandName}>
         MacroSoft
       </a>
-      <button className={styles.hamburger}>
-        {/* icon from heroicons.com */}
+      <button className={styles.hamburger}
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5"
@@ -21,17 +28,33 @@ export default function Navbar() {
           />
         </svg>
       </button>
-      <div
-        className={styles.navigationMenu}>
+      <div className={isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}
+        style={{
+          margin: "auto",
+          display: "flex",
+          padding: 0,
+          listStyleType: "none",
+          margin: "0 1rem",
+          textDecoration: "none",
+          display: "block",
+          width: "100%",
+        }}
+
+      >
         <ul>
           <li>
-            <a href="/home">Home</a>
+            <Link href="/home">
+              <a>
+                Home
+              </a>
+            </Link>
           </li>
           <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
+            <Link href="/home">
+              <a>
+                Home
+              </a>
+            </Link>
           </li>
         </ul>
       </div>
